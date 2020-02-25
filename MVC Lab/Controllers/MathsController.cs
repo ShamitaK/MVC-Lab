2671057e224b60d7878f83dc1853bd31a5e02b59
shamita <shamita.kandru@gmail.com>
2020-02-25 08:32:13 -0500

    Second commit, Added Add Numbers and Divide Numbers Buttons that function

---
+++ MVC Lab/Controllers/MathsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Lab.Models;
+
+namespace MVC_Lab.Controllers
+{
+    public class MathsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddNumbers()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddNumbers(int num1, int num2)
+        {
+            string sum = Maths.Add(num1, num2);
+            return View("Results", sum);
+        }
+
+        [HttpGet]
+        public IActionResult DivideNumbers()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DivideNumbers(int num1, int num2)
+        {
+            string divideNum = Maths.Divide(num1, num2);
+            return View("Results", divideNum);
+        }
+
+        public IActionResult Results()
+        {
+            return View("");
+        }
+
+    }
+}
---
+++ MVC Lab/Models/Maths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Lab.Models
+{
+    public class Maths
+    {
+        
+        public static string Add(int num1, int num2)
+        {
+            int result = (num1 + num2);
+            return result.ToString();
+            
+        }
+
+        public static string Divide(int num1, int num2)
+        {
+            double divisionOfNum = num1 / num2;
+            double remainder = num1 % num2;
+
+            return $"{num1} / {num2} =  {divisionOfNum} Remainder: {remainder}";
+        }
+    }
+}
